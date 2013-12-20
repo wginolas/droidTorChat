875d3cf15963d168c0163b9d9a063c057eccdd1b
Wolfgang Ginolas <you@example.com>
2013-12-20 20:49:23 +0100

    Playing with OnionKit

--- src/eu/gwif/droidtorchat/MainActivity.java
+++ src/eu/gwif/droidtorchat/MainActivity.java
@@ -1,15 +1,22 @@
 package eu.gwif.droidtorchat;
 
+import info.guardianproject.onionkit.ui.OrbotHelper;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private OrbotHelper orbotHelper;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		orbotHelper = new OrbotHelper(getBaseContext());
 	}
 
 	@Override
@@ -18,5 +25,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+    
+    public void onIsRunning(View hello) {
+    	TextView text = (TextView)findViewById(R.id.txtIsRunning);
+    	text.setText(String.format(
+    			"Installed: %s, Running: %s",
+    			orbotHelper.isOrbotInstalled(),
+    			orbotHelper.isOrbotRunning()));
+    	//Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+    }
 }
